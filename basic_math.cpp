063209f00dc3f7dd7a493d9ee7e1ea09c26dab38
possiblybatman <matthew.a.brown01@gmail.com>
2024-09-26 17:46:31 -0400

    Add files via upload

---
+++ basic_math.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// basic_math
+	
+	int addition = 5 + 5;
+	int subtraction = 10 - 5;
+	int division = 10 / 5;
+	int multiplication = 5 * 5;
+	int modulo = 11 % 2;
+	
+	cout << "addition: 5 + 5 = " << addition << endl;
+	cout << "subtraction: 10 - 5 = " << subtraction << endl;
+	cout << "division: 10 / 5 = " << division << endl;
+	cout << "multiplication: 5 * 5 = " << multiplication << endl;
+	cout << "modulo: 11 % 2 = " << modulo << endl;
+	
+			
+	
+	
+}
